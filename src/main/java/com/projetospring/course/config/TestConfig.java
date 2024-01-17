69c9d01a2c547443a5212db19f52cf1be502e608
Yuri Carias <yuristricken7@gmail.com>
2024-01-17 16:21:20 -0300

    OrderItem, many-to-many association with extra attributes

--- src/main/java/com/projetospring/course/config/TestConfig.java
+++ src/main/java/com/projetospring/course/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.projetospring.course.entities.Category;
 import com.projetospring.course.entities.Order;
+import com.projetospring.course.entities.OrderItem;
 import com.projetospring.course.entities.Product;
 import com.projetospring.course.entities.User;
 import com.projetospring.course.entities.enums.OrderStatus;
 import com.projetospring.course.repositories.CategoryRepository;
+import com.projetospring.course.repositories.OrderItemRepository;
 import com.projetospring.course.repositories.OrderRepository;
 import com.projetospring.course.repositories.ProductRepository;
 import com.projetospring.course.repositories.UserRepository;
@@ -33,6 +35,9 @@ public class TestConfig implements CommandLineRunner {
 	
 	@Autowired
 	private ProductRepository productRepository;
+	
+	@Autowired
+	private OrderItemRepository orderItemRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -70,6 +75,13 @@ public class TestConfig implements CommandLineRunner {
 		
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+		
+		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+		OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
+		
+		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi3, oi4));
 		
 		
 	}
--- src/main/java/com/projetospring/course/entities/Order.java
+++ src/main/java/com/projetospring/course/entities/Order.java
@@ -2,7 +2,9 @@ package com.projetospring.course.entities;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.projetospring.course.entities.enums.OrderStatus;
@@ -13,31 +15,33 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "tb_order")
 public class Order implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
+
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
 	private Instant moment;
-	
+
 	private Integer orderStatus;
-	
+
 	@ManyToOne
 	@JoinColumn(name = "client_id")
 	private User client;
 
+	@OneToMany(mappedBy = "id.order")
+	private Set<OrderItem> items = new HashSet<>();
 
 	public Order() {
-		
-	}
 
+	}
 
 	public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
 		super();
@@ -47,31 +51,25 @@ public class Order implements Serializable {
 		this.client = client;
 	}
 
-
 	public Long getId() {
 		return id;
 	}
-
 
 	public void setId(Long id) {
 		this.id = id;
 	}
 
-
 	public Instant getMoment() {
 		return moment;
 	}
 
-
 	public void setMoment(Instant moment) {
 		this.moment = moment;
 	}
-	
 
 	public OrderStatus getOrderStatus() {
 		return OrderStatus.valueOf(orderStatus);
 	}
-
 
 	public void setOrderStatus(OrderStatus orderStatus) {
 		if (orderStatus != null) {
@@ -79,22 +77,22 @@ public class Order implements Serializable {
 		}
 	}
 
-
 	public User getClient() {
 		return client;
 	}
-
 
 	public void setClient(User client) {
 		this.client = client;
 	}
 
+	public Set<OrderItem> getItems() {
+		return items;
+	}
 
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
 	}
-
 
 	@Override
 	public boolean equals(Object obj) {
@@ -107,11 +105,5 @@ public class Order implements Serializable {
 		Order other = (Order) obj;
 		return Objects.equals(id, other.id);
 	}
-	
-	
-	
-	
-	
-	
 
 }
