763df9c033624f13cdeab1fed02535a178532bac
Yuri Carias <yuristricken7@gmail.com>
2024-01-17 21:01:16 -0300

    Payment, one-to-one association

--- src/main/java/com/projetospring/course/config/TestConfig.java
+++ src/main/java/com/projetospring/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.projetospring.course.entities.Category;
 import com.projetospring.course.entities.Order;
 import com.projetospring.course.entities.OrderItem;
+import com.projetospring.course.entities.Payment;
 import com.projetospring.course.entities.Product;
 import com.projetospring.course.entities.User;
 import com.projetospring.course.entities.enums.OrderStatus;
@@ -82,6 +83,11 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2024-01-16T01:50:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 		
 		
 	}
