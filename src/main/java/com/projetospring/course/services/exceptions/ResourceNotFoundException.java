af3b7dccedf01387784dec222dc8c6c343dd6323
Yuri Carias <yuristricken7@gmail.com>
2024-01-17 23:49:00 -0300

    Exception handling - findById

---
+++ src/main/java/com/projetospring/course/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,10 @@
+package com.projetospring.course.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+	
+	public ResourceNotFoundException(Object id) {
+		super("Resouce not found. Id " + id);
+	}
+}
